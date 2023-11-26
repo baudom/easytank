f6a998d52fe868f50cb1dfd29e1d4d8e89f5eded
baudom <dominik@baurecht-welden.de>
2023-11-26 17:46:45 +0100

    change font to 'Nunito'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
+import { Nunito as Font } from "next/font/google";
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import { FC, ReactNode } from "react";
 import { Notifications } from "@mantine/notifications";
 import Footer from "@/components/Footer";
 import { theme } from "@/theme";
+
+const font = Font({
+    subsets: ["latin"],
+    display: "swap",
+    variable: "--font-default",
+});
 
 export const metadata = {
     title: "My Mantine App",
@@ -16,7 +23,10 @@ type Props = {
 
 const Layout: FC<Props> = ({ children }) => {
     return (
-        <html lang="de">
+        <html
+            lang="de"
+            className={font.variable}
+        >
             <head>
                 <ColorSchemeScript />
             </head>
