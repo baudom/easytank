910ede1d58b9208d9890fbd7b0933673af5dc81c
baudom <dominik@baurecht-welden.de>
2024-01-06 16:48:10 +0100

    use track handlers

--- src/components/ColorSchemeToggle/index.tsx
+++ src/components/ColorSchemeToggle/index.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-import { ActionIcon, Box, rem, useMantineColorScheme } from "@mantine/core";
-import { FC } from "react";
+import {
+    ActionIcon,
+    Box,
+    MantineColorScheme,
+    rem,
+    useMantineColorScheme,
+} from "@mantine/core";
+import { FC, useCallback } from "react";
 import { IconMoon, IconSun } from "@tabler/icons-react";
+import useTracking from "@/hooks/useTracking";
 
 const iconStyle = { width: rem(18), height: rem(18) };
 
 const ColorSchemeToggle: FC = () => {
-    const { toggleColorScheme } = useMantineColorScheme();
+    const { toggleColorScheme, colorScheme } = useMantineColorScheme();
+    const { trackThemeChange } = useTracking();
+
+    const onToggleColorScheme = useCallback(
+        (color: MantineColorScheme) => {
+            toggleColorScheme();
+            const currentIsDark = color === "dark";
+            void trackThemeChange(!currentIsDark);
+        },
+        [toggleColorScheme, trackThemeChange],
+    );
 
     return (
         <ActionIcon
-            onClick={toggleColorScheme}
+            onClick={() => onToggleColorScheme(colorScheme)}
             style={{ cursor: "pointer" }}
             variant="light"
             size="lg"
--- src/components/LanguageToggle/index.tsx
+++ src/components/LanguageToggle/index.tsx
@@ -6,19 +6,23 @@ import { useLocale } from "next-intl";
 import { ActionIcon } from "@mantine/core";
 import { DEFAULT_LOCALE } from "@/model/constants";
 import { LocaleType } from "@/model";
+import useTracking from "@/hooks/useTracking";
 
 const LanguageToggle: FC = () => {
     const locale = useLocale();
     const router = useRouter();
+    const { trackLanguageSelect } = useTracking();
     const [, startTransition] = useTransition();
 
     const toggleLanguage = useCallback(() => {
         const nextLocale: LocaleType =
             locale === DEFAULT_LOCALE ? "en" : DEFAULT_LOCALE;
+
+        void trackLanguageSelect(nextLocale);
         startTransition(() => {
             router.replace(`/${nextLocale}`);
         });
-    }, [locale, router]);
+    }, [locale, router, trackLanguageSelect]);
 
     return (
         <ActionIcon
