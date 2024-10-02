e6d5263095a618a6e0142c40411da7ccabee8774
Dominik Baurecht <69344078+baudom@users.noreply.github.com>
2024-10-02 20:37:20 +0200

    fix(#43): Rework tracking dialog (#58)

--- src/model/constants.ts
+++ src/model/constants.ts
@@ -2,7 +2,8 @@ import { LocaleType } from "@/model/index";
 
 export const LS_CAR_CONFIGURATION_KEY = "car-configuration";
 export const LS_STATION_CONFIGURATION_KEY = "station-configuration";
-export const LS_ALLOW_TRACKING = "allow-tracking";
+export const LS_ALLOW_TRACKING_OLD = "allow-tracking";
+export const LS_ALLOW_TRACKING = "tracking-allowed";
 
 export const DEFAULT_LOCALE: LocaleType = "de";
 
