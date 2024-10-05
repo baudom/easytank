ebb60cfd980c50d6de3aca8eee906c64644ea02b
baudom <dominik@baurecht-welden.de>
2024-10-05 23:25:44 +0200

    upgrade deps, use older dark mode and fix layout

--- src/theme.ts
+++ src/theme.ts
@@ -1,10 +1,24 @@
 "use client";
 
-import { createTheme } from "@mantine/core";
+import { createTheme, darken } from "@mantine/core";
 
 export const theme = createTheme({
     fontFamily: "var(--font-default)",
     defaultRadius: "lg",
+    colors: {
+        dark: [
+            "#C1C2C5",
+            "#A6A7AB",
+            "#909296",
+            "#5c5f66",
+            "#373A40",
+            "#2C2E33",
+            "#25262b",
+            "#1A1B1E",
+            "#141517",
+            "#101113",
+        ],
+    },
     defaultGradient: { from: "pink", to: "yellow" },
     /* Put your mantine theme override here */
 });
