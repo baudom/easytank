5ebe2c47118175d43e9ffb09598031d9cf9a0d49
Dominik Baurecht <69344078+baudom@users.noreply.github.com>
2024-10-06 14:40:01 +0200

    perf(#63): UX Improvements and fixes (#64)

--- src/helper/sortings.ts
+++ src/helper/sortings.ts
@@ -4,3 +4,11 @@ export function sortByNumberAsc(a: number, b: number) {
     }
     return a < b ? -1 : 1;
 }
+
+export function sortByStringAsc(a: string, b: string) {
+    if (a === b) {
+        return 0;
+    }
+
+    return a < b ? -1 : 1;
+}
