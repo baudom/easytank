ecf51efa8c9149563e702bd7bf1278558a248a24
baudom <dominik@baurecht-welden.de>
2024-10-06 02:03:17 +0200

    added new tracking keys and events

--- src/hooks/useTracking.tsx
+++ src/hooks/useTracking.tsx
@@ -3,15 +3,26 @@
 import { useCallback } from "react";
 import { useLocalStorage } from "@mantine/hooks";
 import { LS_ALLOW_TRACKING } from "@/model/constants";
+import { FuelType, RadiusType } from "@/model";
 
 const TRACK_LANGUAGE = "language-selector";
 const TRACK_THEME = "theme-selector";
+const TRACK_FUEL_TYPE = "select-fuel-type";
+const TRACK_RADIUS = "select-radius";
 
 export type TrackEventKey =
     | "detect-position"
     | "manual-search"
     | "configure-car"
-    | "install-pwa";
+    | "install-pwa"
+    | "apply-filter-or-sort";
+
+export type InternalTrackEventKey =
+    | TrackEventKey
+    | typeof TRACK_LANGUAGE
+    | typeof TRACK_THEME
+    | typeof TRACK_FUEL_TYPE
+    | typeof TRACK_RADIUS;
 
 const useTracking = () => {
     const [allowTracking, setAllowTracking] = useLocalStorage<
@@ -24,10 +35,7 @@ const useTracking = () => {
     });
 
     const track = useCallback(
-        (
-            key?: TrackEventKey | typeof TRACK_LANGUAGE | typeof TRACK_THEME,
-            props?: object,
-        ) => {
+        (key?: InternalTrackEventKey, props?: object) => {
             if (!allowTracking) return;
             void umami.track(key, props);
         },
@@ -55,10 +63,26 @@ const useTracking = () => {
         [track],
     );
 
+    const trackFuelTypeChange = useCallback(
+        (type: FuelType) => {
+            void track(TRACK_FUEL_TYPE, { type });
+        },
+        [track],
+    );
+
+    const trackRadiusChange = useCallback(
+        (radius: RadiusType) => {
+            void track(TRACK_RADIUS, { radius });
+        },
+        [track],
+    );
+
     return {
         trackEvent,
         trackLanguageSelect,
         trackThemeChange,
+        trackFuelTypeChange,
+        trackRadiusChange,
         allowTracking,
         setAllowTracking,
     };
