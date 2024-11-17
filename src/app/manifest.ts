df7123fb44ecf3ad7acaf2d89b71fd405390a863
Dominik Baurecht <69344078+baudom@users.noreply.github.com>
2024-11-17 13:21:12 +0100

    feat(#67): Shortcuts and Monochrome Icons (#69)

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,7 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
         short_name: "easytank",
         start_url: "/",
         display: "standalone",
-        orientation: "portrait",
+        orientation: "portrait-primary",
         theme_color: "#1864AB",
         background_color: "#1A1B1E",
         description:
@@ -23,21 +23,65 @@ export default function manifest(): MetadataRoute.Manifest {
                 sizes: "192x192",
             },
             {
-                src: "/icon-512.png",
-                type: "image/png",
-                sizes: "512x512",
-            },
-            {
                 src: "/icon-192-maskable.png",
                 type: "image/png",
                 sizes: "192x192",
                 purpose: "maskable",
             },
             {
+                src: "/icon-512.png",
+                type: "image/png",
+                sizes: "512x512",
+            },
+            {
                 src: "/icon-512-maskable.png",
                 type: "image/png",
                 sizes: "512x512",
                 purpose: "maskable",
+            },
+            {
+                src: "/icon-monochrome.png",
+                type: "image/png",
+                sizes: "216x216",
+                purpose: "monochrome",
+            },
+        ],
+        shortcuts: [
+            {
+                name: "Umgebungssuche",
+                short_name: "Umgebungssuche",
+                description: "Suche in der Umgebung mithilfe deines Standortes",
+                url: "/?search-now",
+                icons: [
+                    {
+                        src: "/shortcut-icon/icon-192.png",
+                        type: "image/png",
+                        sizes: "192x192",
+                    },
+                    {
+                        src: "/shortcut-icon/icon-192-maskable.png",
+                        type: "image/png",
+                        sizes: "192x192",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/shortcut-icon/icon-512.png",
+                        type: "image/png",
+                        sizes: "512x512",
+                    },
+                    {
+                        src: "/shortcut-icon/icon-512-maskable.png",
+                        type: "image/png",
+                        sizes: "512x512",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/shortcut-icon/icon-monochrome.png",
+                        type: "image/png",
+                        sizes: "216x216",
+                        purpose: "monochrome",
+                    },
+                ],
             },
         ],
     };
